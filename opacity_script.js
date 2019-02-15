a3be9632a86da15e32a7bbec63a8a2255f003f92
Katerina Panchenko <buddhagolden@gmail.com>
2019-02-15 06:10:59 +0200

    changes 1

--- opacity_script.js
+++ opacity_script.js
@@ -1,27 +1,21 @@
 'use strict';
 var btnPrev = document.querySelector('#gallery .buttons .prev');
 var btnNext = document.querySelector('#gallery .buttons .next');
-var images = document.querySelectorAll('#gallery .photos img'); //получаем массив фотографий
+var images = document.querySelectorAll('#gallery .photos img');
 var i = 0;
-
 btnPrev.onclick = function () {
     images[i].className = '';
     i--;
-
-    //В ПРОГРАММИРОВАНИИ ЭЛЕМЕНТЫ СЧИТАЮТСЯ С 0!!! картинка с номером 0 у нас первая
     if (i < 0) {
         i = images.length - 1;
     }
-
     images[i].className = 'showed';
 };
 btnNext.onclick = function () {
-    images[i].className = ''; //скрываем текущую картинку по классу
+    images[i].className = '';
     i++;
-
     if (i >= images.length) {
         i = 0;
     }
-
-    images[i].className = 'showed'; // открываем следующую ( уже текущую картинку) после скрытой по классу showed
+    images[i].className = 'showed';
 };
